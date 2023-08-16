d47f77a91713d00441bc6f733648be27fb9b2751
Arto Eskelinen <arto.eske@gmail.com>
2023-08-16 09:59:38 +0300

    JS-toimintojen testiä

--- toiminnot.js
+++ toiminnot.js
@@ -1,1 +1,10 @@
-console.log("toimii ok")
+var saldo = 50;
+var panos = 1;
+
+document.getElementById("lompakko").innerHTML = `Lompakostasi löytyy rahaa ${saldo}€.`;
+document.getElementById("panoksesi").value = panos;
+document.getElementById("lukitse1").addEventListener("click", testi);
+
+function testi(){
+    document.getElementsByClassName("rullakuvake")[0].src = "rulla1.gif";
+}
