07527b60b0933bdc008d010d2a2d64626f0f82fa
Arto Eskelinen <arto.eske@gmail.com>
2023-08-17 07:38:32 +0300

    ohje-elementin asettelu

--- toiminnot.js
+++ toiminnot.js
@@ -1,10 +1,13 @@
 var saldo = 50;
 var panos = 1;
+var eraKaksi = false;
 
 document.getElementById("lompakko").innerHTML = `Lompakostasi löytyy rahaa ${saldo}€.`;
 document.getElementById("panoksesi").value = panos;
-document.getElementById("lukitse1").addEventListener("click", testi);
+document.getElementById("lukitse1").addEventListener("click", lukitse1);
 
-function testi(){
-    document.getElementsByClassName("rullakuvake")[0].src = "rulla1.gif";
+function lukitse1(){
+    if(eraKaksi == true){
+        document.getElementsByClassName("rullakuvake")[0].src = "tyhjarulla.gif";
+    }
 }
