0dcda12a73ad127dc55b58b440845e04d07ee997
Arto Eskelinen <arto.eske@gmail.com>
2023-08-17 10:59:48 +0300

    rullien 1. pyöräytys

--- toiminnot.js
+++ toiminnot.js
@@ -1,35 +1,47 @@
 var saldo = 50;
 var panos = 0;
-var eraKaksi = false;
 var lukittu1 = false;
 var lukittu2 = false;
 var lukittu3 = false;
 var lukittu4 = false;
+var eraKaksi = false;
 var otsikko = document.getElementById("otsikko");
+var lompakko = document.getElementById("lompakko");
 
-document.getElementById("lompakko").innerHTML = `Lompakostasi löytyy rahaa ${saldo}€.`;
+lompakko.innerHTML = `Lompakostasi löytyy rahaa ${saldo}€.`;
 document.getElementById("lukitse1").addEventListener("click", lukitse1);
 
 function pelaa(){
-    panos = document.getElementById("panoksesi").value;
-    if(panos < 1 || panos > saldo){
-        otsikko.innerHTML = "Ole hyvä ja aseta kelvollinen panos."
-        document.getElementById("panoksesi").value = 1;
-    } else {
-        otsikko.innerHTML = "Oikein hyvä!"
-        for(indeksi=0; indeksi<4; indeksi++){
-            let luku = Math.round(Math.random()*4);
-            document.getElementsByClassName("rullakuvake")[indeksi].src = `rulla${luku}.gif`;
+    // ensimmäinen pyöräytys
+    if(eraKaksi == false){
+        panos = document.getElementById("panoksesi").value;
+        if(panos < 1 || panos > saldo){
+            otsikko.innerHTML = "Ole hyvä ja aseta kelvollinen panos."
+            document.getElementById("panoksesi").value = 1;
+        } else {
+            otsikko.innerHTML = "Oikein hyvä!"
+            saldo -= panos;
+            lompakko.innerHTML = `Lompakostasi löytyy rahaa ${saldo}€.`;
+            for(indeksi=0; indeksi<4; indeksi++){
+                let luku = Math.round(Math.random()*4);
+                document.getElementsByClassName("rullakuvake")[indeksi].src = `rulla${luku}.gif`;
+            }
+            eraKaksi = true;
         }
+    } else {
+    // toinen pyöräytys
+        console.log("pelasit jo houkka yhden erän")
     }
 }
 
 function lukitse1(){
     if(lukittu1 == true){
-        document.getElementsByClassName("rullakuvake")[0].style.border = "none";
+        document.getElementsByClassName("rullakuvake")[0].style.border = "2px solid black";
+        document.getElementById("lukitse1").innerHTML = "Lukitse";
         lukittu1 = false;
     } else if(eraKaksi == true){
-        document.getElementsByClassName("rullakuvake")[0].style.border = "2px solid blue";
+        document.getElementsByClassName("rullakuvake")[0].style.border = "2px dashed black";
+        document.getElementById("lukitse1").innerHTML = "Avaa";
         lukittu1 = true;
     }
 }
