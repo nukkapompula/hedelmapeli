77b33f4d46fbb7575c7c0a81fcfa7ef904d5f190
Arto Eskelinen <arto.eske@gmail.com>
2023-08-18 10:59:39 +0300

    tarkistusfunktio

--- toiminnot.js
+++ toiminnot.js
@@ -6,10 +6,10 @@ var otsikko = document.getElementById("otsikko");
 var lompakko = document.getElementById("lompakko");
 
 lompakko.innerHTML = `Lompakostasi löytyy rahaa ${saldo}€.`;
-document.getElementById("lukitse0").addEventListener("click", lukitse);
-document.getElementById("lukitse1").addEventListener("click", lukitse);
-document.getElementById("lukitse2").addEventListener("click", lukitse);
-document.getElementById("lukitse3").addEventListener("click", lukitse);
+document.getElementById("lukitse0").addEventListener("click", lukitseTaiAvaa);
+document.getElementById("lukitse1").addEventListener("click", lukitseTaiAvaa);
+document.getElementById("lukitse2").addEventListener("click", lukitseTaiAvaa);
+document.getElementById("lukitse3").addEventListener("click", lukitseTaiAvaa);
 
 function pelaa(){
     // ensimmäinen pyöräytys
@@ -19,13 +19,12 @@ function pelaa(){
             otsikko.innerHTML = "Ole hyvä ja aseta kelvollinen panos.";
             document.getElementById("panoksesi").value = 1;
         } else {
-            otsikko.innerHTML = "Oikein hyvä!"
-            saldo -= panos;
             lompakko.innerHTML = `Lompakostasi löytyy rahaa ${saldo}€.`;
             for(indeksi=0; indeksi<4; indeksi++){
                 let luku = Math.round(Math.random()*4);
                 document.getElementsByClassName("rullakuvake")[indeksi].src = `rulla${luku}.gif`;
             }
+            voitonTarkistus();
             eraKaksi = true;
         }
     } else {
@@ -35,7 +34,6 @@ function pelaa(){
             otsikko.innerHTML = "Ole hyvä ja aseta kelvollinen panos.";
             document.getElementById("panoksesi").value = 1;
         } else {
-            saldo -= panos;
             lompakko.innerHTML = `Lompakostasi löytyy rahaa ${saldo}€.`;
             for(indeksi=0; indeksi<4; indeksi++){
                 luku = Math.round(Math.random()*4);
@@ -43,11 +41,13 @@ function pelaa(){
                     document.getElementsByClassName("rullakuvake")[indeksi].src = `rulla${luku}.gif`;
                 }
             }
+            voitonTarkistus();
         }
     }
 }
 
-function lukitse(event){
+function lukitseTaiAvaa(event){
+    // kohteen id:stä saadaan käsiteltävän lukon numero
     let kohde = Number(event.target.id[7]);
     if(lukossa[kohde] == true){
         document.getElementsByClassName("rullakuvake")[kohde].style.border = "2px solid black";
@@ -58,4 +58,36 @@ function lukitse(event){
         document.getElementById(`lukitse${kohde}`).innerHTML = "Avaa";
         lukossa[kohde] = true;
     }
+}
+
+function voitonTarkistus(){
+    saldo -= panos;
+    lompakko.innerHTML = `Lompakostasi löytyy rahaa ${saldo}€.`;
+
+    let vihreat = 0;
+    let siniset = 0;
+    let oranssit = 0;
+    let turkoosit = 0;
+    let punaiset = 0;
+
+    // lasketaan kunkin värin osumien määrä
+    for(indeksi=0; indeksi<4; indeksi++){
+        let verrokki = document.getElementsByClassName("rullakuvake")[indeksi].src.toString();
+        if(verrokki.substring(22, 32) == "rulla0.gif"){
+            vihreat += 1;
+        }
+        if(verrokki.substring(22, 32) == "rulla1.gif"){
+            siniset += 1;
+        }
+        if(verrokki.substring(22, 32) == "rulla2.gif"){
+            oranssit += 1;
+        }
+        if(verrokki.substring(22, 32) == "rulla3.gif"){
+            turkoosit += 1;
+        }
+        if(verrokki.substring(22, 32) == "rulla4.gif"){
+            punaiset += 1;
+        }
+    }
+    console.log(vihreat, siniset, oranssit, turkoosit, punaiset);
 }
