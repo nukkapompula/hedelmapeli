13c552f2c99b1223e863e8373c910640201ca08c
Arto Eskelinen <arto.eske@gmail.com>
2023-08-18 13:01:05 +0300

    voiton tarkistaminen

--- toiminnot.js
+++ toiminnot.js
@@ -19,7 +19,6 @@ function pelaa(){
             otsikko.innerHTML = "Ole hyvä ja aseta kelvollinen panos.";
             document.getElementById("panoksesi").value = 1;
         } else {
-            lompakko.innerHTML = `Lompakostasi löytyy rahaa ${saldo}€.`;
             for(indeksi=0; indeksi<4; indeksi++){
                 let luku = Math.round(Math.random()*4);
                 document.getElementsByClassName("rullakuvake")[indeksi].src = `rulla${luku}.gif`;
@@ -34,7 +33,6 @@ function pelaa(){
             otsikko.innerHTML = "Ole hyvä ja aseta kelvollinen panos.";
             document.getElementById("panoksesi").value = 1;
         } else {
-            lompakko.innerHTML = `Lompakostasi löytyy rahaa ${saldo}€.`;
             for(indeksi=0; indeksi<4; indeksi++){
                 luku = Math.round(Math.random()*4);
                 if(lukossa[indeksi] == 0){
@@ -61,9 +59,6 @@ function lukitseTaiAvaa(event){
 }
 
 function voitonTarkistus(){
-    saldo -= panos;
-    lompakko.innerHTML = `Lompakostasi löytyy rahaa ${saldo}€.`;
-
     let vihreat = 0;
     let siniset = 0;
     let oranssit = 0;
@@ -72,22 +67,46 @@ function voitonTarkistus(){
 
     // lasketaan kunkin värin osumien määrä
     for(indeksi=0; indeksi<4; indeksi++){
-        let verrokki = document.getElementsByClassName("rullakuvake")[indeksi].src.toString();
-        if(verrokki.substring(22, 32) == "rulla0.gif"){
+        let pysahtynytRulla = document.getElementsByClassName("rullakuvake")[indeksi].src.toString();
+        if(pysahtynytRulla.substring(22, 32) == "rulla0.gif"){
             vihreat += 1;
         }
-        if(verrokki.substring(22, 32) == "rulla1.gif"){
+        if(pysahtynytRulla.substring(22, 32) == "rulla1.gif"){
             siniset += 1;
         }
-        if(verrokki.substring(22, 32) == "rulla2.gif"){
+        if(pysahtynytRulla.substring(22, 32) == "rulla2.gif"){
             oranssit += 1;
         }
-        if(verrokki.substring(22, 32) == "rulla3.gif"){
+        if(pysahtynytRulla.substring(22, 32) == "rulla3.gif"){
             turkoosit += 1;
         }
-        if(verrokki.substring(22, 32) == "rulla4.gif"){
+        if(pysahtynytRulla.substring(22, 32) == "rulla4.gif"){
             punaiset += 1;
         }
     }
-    console.log(vihreat, siniset, oranssit, turkoosit, punaiset);
+
+    // tarkistetaan osumien perusteella voitot
+    if(vihreat == 4){
+        saldo += 3 * panos;
+        otsikko.innerHTML = `Voitit juuri ${3 * panos}€!`;
+    } else if(siniset == 4){
+        saldo += 4 * panos;
+        otsikko.innerHTML = `Voitit juuri ${4 * panos}€!`;
+    } else if(oranssit == 4){
+        saldo += 5 * panos;
+        otsikko.innerHTML = `Voitit juuri ${5 * panos}€!`;
+    } else if(turkoosit == 4){
+        saldo += 6 * panos;
+        otsikko.innerHTML = `Voitit juuri ${6 * panos}€!`;
+    } else if(punaiset == 3){
+        saldo += 5 * panos;
+        otsikko.innerHTML = `Voitit juuri ${5 * panos}€!`;
+    } else if(punaiset == 4){
+        saldo += 10 * panos;
+        otsikko.innerHTML = `Voitit juuri ${10 * panos}€!`;
+    } else {
+        saldo -= panos;
+        otsikko.innerHTML = "Himskatti, ei voittoa.";
+    }
+    lompakko.innerHTML = `Lompakostasi löytyy rahaa ${saldo}€.`;
 }
